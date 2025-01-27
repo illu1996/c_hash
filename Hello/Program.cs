9a7c0a915a0d0d130e9e39c98469cd7cc102df49
Lee Ji Hyeok <illu140411@gmail.com>
2025-01-28 00:14:24 +0900

    c sharp study

---
+++ Hello/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if ( args.Length == 0 )
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+            Console.WriteLine("Hello, {0}!", args[0]);
+        }
+    }
+}
