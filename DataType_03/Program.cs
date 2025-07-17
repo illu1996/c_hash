3dbf8ecec65dead7355a9ef860ff8d18a69b8075
Jerry <illu140411@gmail.com>
2025-07-17 22:47:29 +0900

    Done Chapter 3

--- DataType_03/Program.cs
+++ DataType_03/Program.cs
@@ -2,6 +2,10 @@
 {
     internal class Program
     {
+        enum DialogResult { YES, NO, CANCEL, CONFIRM, WAIT };
+        enum DialogResult1 { YES = 20, NO, CANCEL, CONFIRM = 40, WAIT };
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("----------데이터 정수 타입----------");
@@ -111,7 +115,87 @@
             Console.WriteLine(ob4);
             Console.WriteLine("----------구분선----------");
 
+            Console.WriteLine("----------Boxing & UnBoxing----------");
+            int int_a = 123;
+            Object obj = (Object)int_a; //int_a 값을 박싱해서 힙에 저장
+            int int_c = (int)obj; //obj에 담긴 값을 언박싱
+            Console.WriteLine("---------구분선----------");
 
+            Console.WriteLine("----------형변환----------");
+            int int_12345 = 12345;
+            float f_1234 = 0.234f;
+            string string_12345 = int_12345.ToString();
+            string string_1234 = f_1234.ToString();
+            int convert_int = int.Parse(string_12345);
+            float convert_float = float.Parse(string_1234);
+            Console.WriteLine($"{int_12345},{string_12345},{f_1234},{string_1234},{convert_int},{convert_float}");
+            Console.WriteLine("----------구분선----------");
+
+            Console.WriteLine("----------상수와 열거형식----------");
+            const int const1 = 1;
+            const double const2 = 3.14;
+            const string const3 = "abcdef";
+            Console.WriteLine($"{const1},{const2},{const3}");
+            //const int const1 = 4; // 에러표시
+            //Console.WriteLine(const1);
+            const int MAX_INT = int.MaxValue;
+            const int MIN_INT = int.MinValue;
+
+            //상수를 이용한 열거처럼 사용
+            const int RESULT_OK = 1;
+            const int RESULT_NO = 2;
+            const int RESULT_CONFIRM = 3;
+            const int RESULT_CANCLE = 4;
+            const int RESULT_WAIT = 5;
+
+            //열거 
+            Console.WriteLine((int)DialogResult.YES);
+            Console.WriteLine(DialogResult.NO);
+            Console.WriteLine((int)DialogResult.CANCEL);
+            Console.WriteLine(DialogResult.CONFIRM);
+            Console.WriteLine(DialogResult.WAIT);
+
+            Console.WriteLine("----------구분선----------");
+            Console.WriteLine("----------Nullable----------");
+            int? null_a = null;
+            float? null_b = null;
+            double? null_c = null;
+
+            Console.WriteLine(null_a.HasValue); // False
+            null_a = 3;
+            Console.WriteLine(null_a.HasValue); // True
+            Console.WriteLine(null_a.Value); // 3
+
+            int null_d; //이는 nullable 형식도 아닐뿐더러 0으로 C#이 초기화 시켜버린다.
+
+            Console.WriteLine("----------구분선----------");
+            Console.WriteLine("---------- var ----------");
+            var var_a = 20;//var는 반드시 선언과 동시에 초기화
+            var var_b = 3.1414213;
+            var var_c = "Hello,World";
+            var var_d = new int[] { 10, 20, 30 };
+
+            Console.WriteLine("Type:{0}, Value{1}", var_a.GetType(), var_a); //Type: System.Int32, Value20
+            Console.WriteLine("Type:{0}, Value{1}", var_b.GetType(), var_b); //Type:System.Double, Value3.1414213
+            Console.WriteLine("Type:{0}, Value{1}", var_c.GetType(), var_c);// Type: System.String, ValueHello,World
+            foreach ( var var_e in var_d)
+            {
+                Console.WriteLine("Type:{0}, Value{1}", var_e.GetType(), var_e);
+
+            }
+
+            Console.WriteLine("----------구분선----------");
+            Console.WriteLine("---------- 문자열 포매터----------");
+            Console.WriteLine("Total : {0,-7:D}DEF", "ABC"); //{첨자, 맞춤, 서식문자열}
+            //Total : ABC       DEF DEF의 칸 지정
+
+            string fmt = "{0,-20}{1,-15}{2,30}";
+            Console.WriteLine(fmt, "Publisher", "Author","Title");
+            Console.WriteLine(fmt, "Marvle", "Stan Lee","Iron Man");
+            Console.WriteLine(fmt, "Hanbit", "Sanghyun Park","This is C#");
+            //Publisher           Author                                  Title
+            //Marvle              Stan Lee                             Iron Man
+            //Hanbit              Sanghyun Park                      This is C#
         }
     }
 }
